61e19068632235d2f169ad637dd85cd5c75da9b8
Andrew D. Yates <andrewyates.name@gmail.com>
2013-01-27 19:00:13 -0500

    submit kang norm

--- kang.R
+++ kang.R
@@ -1,3 +1,10 @@
+# qsub -I -l walltime=8:00:00 -l nodes=1;ppn=12
 library(SCAN.UPC)
 library(huex10stv2hsentrezgprobe)
 library(huex10stv2hsentrezg.db)
+library(pd.huex.1.0.st.v2)
+
+fname.ptn <- "/nfs/01/osu6683/kang_gse25219_raw/DFC_over13/*.CEL.gz"
+KANG.SCAN = SCAN(fname.ptn, probeSummaryPackage=huex10stv2hsentrezgprobe, outFilePath="KANG.SCAN.txt")
+KANG.UPC = UPC(fname.ptn, probeSummaryPackage=huex10stv2hsentrezgprobe, outFilePath="KANG.UPC.txt")
+save(KANG.SCAN, KANG.UPC, file="KANG.RData")
